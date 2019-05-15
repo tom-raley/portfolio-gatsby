ff387b3092bc4cb4cda1913c7b3c9c5afa8ee5a1
Tom Raley <tom.raley@gmail.com>
2019-05-15 16:45:54 -0500

    added markdown support

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,4 @@
 import React, { Component } from "react"
-/* import { Link } from "gatsby"
-import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa" */
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Navbar from "../components/Navbar"
