19bfc384931b1d8dae638361155899a6a163e8e0
Tom Raley <tom.raley@gmail.com>
2019-05-15 16:06:47 -0500

    added blog coming soon message

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,9 +7,9 @@ import Navbar from "../components/Navbar"
 class Blog extends Component {
   render() {
     return <Layout>
-      <SEO title="404: Not found" />
+      <SEO title="blog" />
       <Navbar />
-      
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh'}}><h3>Coming Soon!</h3></div>
   </Layout>
   }
 }
