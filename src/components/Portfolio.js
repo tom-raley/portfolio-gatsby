4b57cda34469dae5aa691afc0dddf3cb38086bf7
usnlmc07m07gdy3g <usnlnc07m07gdy3g@Mac-Mini1.local>
2019-05-16 16:06:11 -0500

    compressed images and made buttons more accessible

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 
 const beeristImg = require('../dist/images/beerist.png')
 const beeristWebp = require('../dist/images/beerist.webp')
+const beeristMobile = require('../dist/images/beerist-mobile.png')
 const offTheRalesImg = require('../dist/images/offtherales.png')
 const offTheRalesWebp = require('../dist/images/offtherales.webp')
+const offTheRalesMobile = require('../dist/images/offtherales-mobile.png')
 const squareOneImg = require('../dist/images/squareone.png')
 const squareOneWebp = require('../dist/images/squareone.webp')
+const squareOneMobile = require('../dist/images/squareone-mobile.png')
 
 
 class Portfolio extends Component {
@@ -18,12 +21,13 @@ class Portfolio extends Component {
             <div className="card">
               <picture data-aos="fade-right" data-aos-duration="2000">
                 <source className="card-img-top" srcSet={squareOneWebp} />
+                <source className="card-img-top" media="(min-width: 780px)" srcSet={squareOneMobile} />
                 <img className="card-img-top" src={squareOneImg} alt="Square One screenshot" />
               </picture>
               <div className="card-body" data-aos="fade-left" data-aos-duration="2000">
                 <h5 className="card-title">Square One Mission</h5>
                 <p className="card-text">A fantastic local company, Square One LLC develops and produces sustainable products that are reusable and environmentally-friendly. When they needed a site to send their customers to, I jumped at the chance to design and develop this landing page for them using Bootstrap, a bit of JavaScript, and HTML/CSS.</p>
-                <a href="https://squareonemission.com" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">View</a>
+                <a href="https://squareonemission.com" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">Visit Site</a>
               </div>
             </div>
           </div>
@@ -35,12 +39,13 @@ class Portfolio extends Component {
             <div className="card">
               <picture data-aos="fade-right" data-aos-duration="2000">
                 <source className="card-img-top" srcSet={offTheRalesWebp} />
+                <source className="card-img-top" media="(min-width: 780px)" srcSet={offTheRalesMobile} />
                 <img className="card-img-top" src={offTheRalesImg} alt="Off the rales screenshot" />
               </picture>
               <div className="card-body" data-aos="fade-left" data-aos-duration="2000">
                 <h5 className="card-title">Off the Rales Homebrewery</h5>
                 <p className="card-text">In addition to web development, I am an avid homebrewer.  I built my own website to showcase my homebrewed beers.</p>
-                <a href="https://tom-raley.github.io/offtherales/" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">View</a>
+                <a href="https://tom-raley.github.io/offtherales/" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">Visit Site</a>
               </div>
             </div>
           </div>
@@ -51,12 +56,13 @@ class Portfolio extends Component {
               <div className="card" >
                 <picture  data-aos="fade-right" data-aos-duration="2000">
                   <source className="card-img-top" srcSet={beeristWebp} />
+                  <source className="card-img-top" media="(min-width: 780px)" srcSet={beeristMobile} />
                   <img className="card-img-top" src={beeristImg} alt="Beerist screenshot" />
                 </picture>
                 <div className="card-body" data-aos="fade-left" data-aos-duration="2000">
                   <h5 className="card-title">Beerist</h5>
                   <p className="card-text">A single page beer recommendation web app that uses JavaScript to take a user input, interface with the Untappd API, and return a percentage chance of the user liking that beer.</p>
-                  <a href="https://tom-raley.github.io/beerist/" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">View</a>
+                  <a href="https://tom-raley.github.io/beerist/" target="_blank" rel="noopener noreferrer" className="btn btn-primary btn-name-color">Visit Site</a>
                 </div>
               </div>
             </div>
