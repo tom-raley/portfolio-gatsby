185b53aa52c7dcc41733f09cac68a19fde881850
Tom Raley <tom.raley@gmail.com>
2019-05-17 11:41:28 -0500

    refactored navbar component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,71 +1,54 @@
 import React, { Component } from "react"
 import { Link } from 'gatsby'
 import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
 class Navigation extends Component {
-  render() {
-    return <Navbar collapseOnSelect className="navbar sticky-top navbar-expand-lg navbar-light bg-light" expand="lg" bg="light" id="nav">
-      <Link
-      to="/"
-      className="navbar-brand" 
-      id="home-button"
-      >
-        tom<span className="name-color">raley</span>
-      </Link>
-      {//Need to make mobile menu collapse when clicking link and make animation work
-      <Navbar.Toggle className="hamburger hamburger--spin navbar-toggler collapsed" id="mobile-nav-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-        aria-expanded="false" aria-label="Toggle navigation">
-        <span className="hamburger-box">
-          <span className="hamburger-inner"></span>
-        </span>
-      </Navbar.Toggle>
+  
+  componentDidMount() {
+    //Makes X change to hamburger on click of link inside of nav menu
+    /* $(".nav-link:not(#navbarDropdown)").click(function () {
+      $("#mobile-nav-button").toggleClass("is-active");
+    })
 
-      <Navbar.Collapse className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-          <Link 
-          to="#about"
-          className="nav-link"
-          >
-            About<span className="sr-only">(current)</span>
-          </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="#portfolio"
-              className="nav-link"
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="#contact"
-              className="nav-link"
-            >
-              Contact
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/blog"
-              className="nav-link"
-            >
-              Blog
-            </Link>
-          </li>
-        </ul>
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link social-logo fab fa-linkedin fa-2x" id="nav-link-color" href="https://www.linkedin.com/in/tom-raley/" target="_blank" rel="noopener noreferrer"><i alt="facebook logo"></i></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link social-logo fab fa-twitter-square fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener noreferrer"><i alt="twitter logo"></i></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link social-logo fab fa-github-square fa-2x" id="nav-link-color" href="https://github.com/tom-raley" target="_blank" rel="noopener noreferrer"><i alt="github logo"></i></a>
-          </li>
-        </ul>
+    //If Collapses navbar when clicking on tomraley when mobile navbar is shown
+    $("#home-button").click(function () {
+      $("#mobile-nav-button").toggleClass("is-active");
+      $("#navbarSupportedContent").collapse("hide");
+    })
+
+    //Makes hamburger change to X on click of hamburger button and back on click
+    $("#mobile-nav-button").click(function () {
+      $("#mobile-nav-button").toggleClass("is-active");
+    });
+
+    //Makes menu close when a link is clicked within menu
+    $("#navbarSupportedContent").on('show.bs.collapse', function () {
+      $('.nav-link:not(#navbarDropdown)').click(function () {
+        $("#navbarSupportedContent").collapse('hide');
+      });
+    }); */
+  } 
+  render() {
+    return <Navbar collapseOnSelect className="sticky-top" expand="lg" bg="light" id="nav">
+      <Navbar.Brand id="home-button"><Link className="navbar-brand" to="/#home">tom<span className="name-color">raley</span></Link></Navbar.Brand>
+        <Navbar.Toggle className="hamburger hamburger--spin" id="mobile-nav-button">
+          <span className="hamburger-box">
+            <span className="hamburger-inner"></span>
+          </span>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="navbarSupportedContent">
+          <Nav className="mr-auto">
+            <Nav.Link><Link className="nav-link" to="#about">About</Link></Nav.Link>
+            <Nav.Link><Link className="nav-link" to="#portfolio">Portfolio</Link></Nav.Link>
+            <Nav.Link><Link className="nav-link" to="#contact">Contact</Link></Nav.Link>
+            <Nav.Link><Link className="nav-link" to="/blog">Blog</Link></Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className="social-logo fab fa-linkedin fa-2x" id="nav-link-color" href="https://www.linkedin.com/in/tom-raley/" target="_blank"></Nav.Link>
+            <Nav.Link className="social-logo fab fa-twitter-square fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank"></Nav.Link>
+            <Nav.Link className="social-logo fab fa-github-square fa-2x" id="nav-link-color" href="https://github.com/tom-raley" target="_blank"></Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   } 
