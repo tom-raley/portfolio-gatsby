b3653a5b15486c01ebccf7a5287638d4ed2270a1
tom-raley <tom.raley@gmail.com>
2019-05-21 22:28:01 -0500

    created blog page, post template, made first post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,80 @@
-import React, { Component } from 'react'
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import React from "react";
+import Link from "gatsby-link";
 import Navigation from "../components/Navigation"
+import { graphql } from "gatsby"
 
-
-class Blog extends Component {
-  render() {
-    return <Layout>
-      <SEO title="blog" />
-      <Navigation />
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh'}}><h3>Coming Soon!</h3></div>
-  </Layout>
+export default function Index({ data }) {
+  const { edges: posts } = data.allMarkdownRemark;
+  const postListStyle = {
+    paddingTop: '3em',
+    paddingBottom: '1em',
+    marginBottom: '1em',
   }
-}
 
-export default Blog
+  const previewStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    marginLeft: '15em',
+    marginRight: '15em',
+    borderBottom: '2px solid #e6e6e6',
+  }
+
+  const titleStyle = {
+    fontSize: '2em',
+    textDecoration: 'none',
+    color: '#0981E5'
+  }
+
+  const dateStyle = {
+    fontSize: '0.2em !important',
+    fontWeight: '100 !important'
+  }
+  return (
+    <React.Fragment>
+      <Navigation />
+      <div className="blog-posts" style={postListStyle}>
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }) => {
+            return (
+              <div className="blog-post-preview" key={post.id} style={previewStyle}>
+                <h1 style={titleStyle}>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h5 style={dateStyle}>{post.frontmatter.date}</h5>
+                <p>{post.excerpt}</p>
+              </div>
+            );
+          })}
+      </div>
+    </React.Fragment>
+  );
+}
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          excerpt(pruneLength: 250)
+          id
+          frontmatter {
+            title
+            date
+            path
+            image {
+              childImageSharp {
+                resize(width: 1500, height: 1500) {
+                  src
+                }
+                fluid(maxWidth: 786) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 import Navigation from "../components/Navigation"
+import Footer from "../components/Footer"
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -8,18 +10,49 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
 
+  const blogPostContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  }
+  const titleStyle = {
+    textAlign: 'center',
+    paddingTop: '1em',
+    fontWeight: '500'
+  }
+  const dateStyle = {
+    textAlign: 'center',
+    paddingTop: '0.5em',
+    paddingBottom: '3em',
+    fontSize: '1em'
+  }
+
+  const blogPostContentStyle = {
+    paddingTop: '3em',
+    marginLeft: '15em',
+    marginRight: '15em',
+    lineHeight: '2em'
+  }
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <Navigation />
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
+    <React.Fragment>
+    <Navigation />
+      <div className="blog-post-container">
+        <div className="blog-post" style={blogPostContainerStyle}>
+          <h1 style={titleStyle}>{frontmatter.title}</h1>
+          <h2 style={dateStyle}>{frontmatter.date}</h2>
+          <figure>
+            <Img fluid={frontmatter.image.childImageSharp.fluid} />
+            <figcaption>{frontmatter.credit}</figcaption>
+          </figure>
+          <div
+            className="blog-post-content"
+            style={blogPostContentStyle}
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </React.Fragment>
   )
 }
 
@@ -28,9 +61,19 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
+        date
         path
         title
+        image {
+          childImageSharp {
+            resize(width: 1500, height: 1500) {
+              src
+            }
+            fluid(maxWidth: 786) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+       }
       }
     }
   }
