a8f3d6ef90c7c1ed684ea20a532b3809d1e4e230
Tom Raley <tomraley@toms-mbp.attlocal.net>
2019-06-13 20:18:07 -0500

    changed jumbotron animation from css to js

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -3,22 +3,33 @@ import Img from "gatsby-image"
 import Center from 'react-center'
 
 class Jumbotron extends Component {
+
+  componentDidMount() {
+    const AOS = require('aos');
+    this.aos = AOS
+    this.aos.init()
+  }
+
+  componentDidUpdate() {
+    this.aos.refresh()
+  }
+
+
   render() {
     const profilePic = this.props.profilePic.childImageSharp.fluid
     const profilePicStyle = {
       borderRadius: "50%",
       width: "300px",
-      animation: "3s 0s 1 fadeIn",
       textAlign: "center"
     }
     return <div className="jumbotron jumbotron-fluid" id="home">
       <div className="container">
         <Center>
-          <Img fluid={profilePic} style={profilePicStyle} alt="tom" />
+          <Img fluid={profilePic} style={profilePicStyle} alt="tom" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0"/>
         </Center>
-        <h1 className="display-4">Hi, I'm Tom.</h1>
-        <p className="lead">I'm passionate about solving problems and creating beautiful, functional solutions. </p>
-        <p className="lead">My tools are HTML, CSS and JavaScript.</p>
+        <h1 data-aos="fade-up" data-aos-duration="1000" className="display-4">Hi, I'm Tom.</h1>
+        <p className="lead" data-aos="fade-up" data-aos-duration="2000">I'm passionate about solving problems and creating beautiful, functional solutions. </p>
+        <p className="lead" data-aos="fade-up" data-aos-duration="2000">My tools are HTML, CSS and JavaScript.</p>
       </div>
     </div>
   }
