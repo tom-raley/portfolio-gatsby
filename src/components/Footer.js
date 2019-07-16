41923bfd7f5866ff2ba2f4620fb346bb38e22aac
tom-raley <tom.raley@gmail.com>
2019-07-15 21:36:23 -0500

    updated accessibility of links, sped up animation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ class Footer extends Component {
   render() {
     return <footer>
       <div className="footer-social">
-        <a className="social-logo fab fa-linkedin fa-2x" id="nav-link-color" href="https://www.linkedin.com/in/tom-raley/" target="_blank" rel="noopener noreferrer"><FaLinkedin size={32} /></a>
-        <a className="social-logo fab fa-twitter-square fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener noreferrer"><FaTwitterSquare size={32} /></a>
-        <a className="social-logo fab fa-github-square fa-2x" id="nav-link-color" href="https://github.com/tom-raley" target="_blank" rel="noopener noreferrer"><FaGithubSquare size={32} /></a>
+        <a className="social-logo fab fa-linkedin fa-2x" id="nav-link-color" href="https://www.linkedin.com/in/tom-raley/" target="_blank" rel="noopener noreferrer"><span class="visually-hidden">LinkedIn</span><FaLinkedin size={32} /></a>
+        <a className="social-logo fab fa-twitter-square fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener noreferrer"><span class="visually-hidden">Twitter</span><FaTwitterSquare size={32} /></a>
+        <a className="social-logo fab fa-github-square fa-2x" id="nav-link-color" href="https://github.com/tom-raley" target="_blank" rel="noopener noreferrer"><span class="visually-hidden">GitHub</span><FaGithubSquare size={32} /></a>
       </div>
     </footer>
   }
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -25,9 +25,9 @@ class Jumbotron extends Component {
     return <div className="jumbotron jumbotron-fluid" id="home">
       <div className="container">
         <Center>
-          <Img fluid={profilePic} style={profilePicStyle} alt="tom" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="300" data-aos-offset="0"/>
+          <Img fluid={profilePic} style={profilePicStyle} alt="tom" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="2000" data-aos-offset="0"/>
         </Center>
-        <h1 data-aos="fade-up" data-aos-duration="1000" className="display-4">Hi, I'm Tom.</h1>
+        <h1 className="display-4">Hi, I'm Tom.</h1>
         <p className="lead" data-aos="fade-up" data-aos-duration="2000">I'm passionate about solving problems and creating beautiful, functional solutions. </p>
         <p className="lead" data-aos="fade-up" data-aos-duration="2000">My tools are HTML, CSS and JavaScript.</p>
       </div>
