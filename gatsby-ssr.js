1db9096dafd13ed4058ce7cfe720ac3de035b914
tom-raley <tom.raley@gmail.com>
2019-07-15 21:42:25 -0500

    trying to preconnect GA

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -11,9 +11,15 @@ import React from 'react'
 export const onRenderBody = ({ setHeadComponents }) => {
     setHeadComponents([
       <link
+        rel="preconnect"
+        key="preconnect-google-analytics"
+        href="https://www.google-analytics.com"
+      />,
+      <link
         rel="dns-prefetch"
         key="dns-prefetch-google-analytics"
         href="https://www.google-analytics.com"
       />,
     ]);
   };
+
