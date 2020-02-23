9093a8c9f21ef2d83bf9aa3d24a8d41e9a9f8e3e
tom-raley <tom.raley@gmail.com>
2020-02-22 22:15:38 -0600

    fixes filesystem error

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/posts/`,
-        name: 'posts',
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: 'images',
         path: `${__dirname}/src/dist/images`,
       }
