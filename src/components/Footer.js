fa7f309d58b6cb308e2a036560fa9c0afd1867fa
thomas.raley <thomas.raley@fleetcor.com>
2023-12-07 13:29:39 -0700

    update twitter to x

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react"
-import { FaTwitterSquare, FaGithubSquare } from 'react-icons/fa'
-// import { FaTwitterSquare, FaGithubSquare, FaLinkedin } from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6';
 
 class Footer extends Component {
   render() {
     return <footer>
       <div className="footer-social overflow-hidden">
         {/* <a className="social-logo fab fa-linkedin fa-2x" id="nav-link-color" href="https://www.linkedin.com/in/tom-raley/" target="_blank" rel="noopener noreferrer"><span className="visually-hidden">LinkedIn</span><FaLinkedin size={32} /></a> */}
-        <a className="social-logo fab fa-twitter-square fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener noreferrer"><span className="visually-hidden">Twitter</span><FaTwitterSquare size={32} /></a>
+        <a className="social-logo fab fa-x-twitter fa-2x" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener noreferrer"><span className="visually-hidden">Twitter</span><FaXTwitter size={32} /></a>
         <a className="social-logo fab fa-github-square fa-2x" id="nav-link-color" href="https://github.com/tom-raley" target="_blank" rel="noopener noreferrer"><span className="visually-hidden">GitHub</span><FaGithubSquare size={32} /></a>
       </div>
     </footer>
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,7 +3,8 @@ import { Link } from 'gatsby'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
-import { FaTwitterSquare, FaGithubSquare, FaLinkedin} from 'react-icons/fa'
+import { FaGithubSquare } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6';
 
 class Navigation extends Component {
    constructor(props) {
@@ -47,7 +48,7 @@ class Navigation extends Component {
             <Navbar.Toggle onClick={() => this.toggleClass()} role="button"><Link className="nav-link" role="button" to="/blog">Blog</Link></Navbar.Toggle>
           </Nav>
           <Nav>
-          <Nav.Link className="social-logo" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener"><FaTwitterSquare size={32} /><span className="visually-hidden">Twitter</span></Nav.Link>
+          <Nav.Link className="social-logo" id="nav-link-color" href="https://twitter.com/tom_raley" target="_blank" rel="noopener"><FaXTwitter size={32} /><span className="visually-hidden">Twitter</span></Nav.Link>
           <Nav.Link className="social-logo" id="nav-link-color" href="https://github.com/tom-raley" target="_blank" rel="noopener"><FaGithubSquare size={32} /><span className="visually-hidden">GitHub</span></Nav.Link>
         </Nav>
       </Navbar.Collapse>
